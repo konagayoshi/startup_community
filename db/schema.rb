a27d3b05b9d57cdfa5e2914afb33d4b0c1bb6d2b
duynl_58 <duynl_58@vnu.edu.vn>
2015-10-28 21:54:01 +0700

    Add students' profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020185356) do
+ActiveRecord::Schema.define(version: 20151028141248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20151020185356) do
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.string   "president"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.string   "university"
+    t.string   "major"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
