35b3ea1dcb377acf39a96501bb9209eaf83e0f24
vagrant <hikarushinomiya2080085@gmail.com>
2015-11-21 06:45:18 +0000

    Commented out files for test.

--- test/controllers/searchsome_controller_test.rb
+++ test/controllers/searchsome_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class SearchsomeControllerTest < ActionController::TestCase
-  test "should get searchtoplist" do
-    get :searchtoplist
-    assert_response :success
-  end
+#  test "should get searchtoplist" do
+#    get :searchtoplist
+#    assert_response :success
+#  end
 
 #  test "should get searchdetail" do
 #    get :searchdetail
