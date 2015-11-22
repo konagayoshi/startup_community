d49e0785244e3265850d5e1acea40bbf0528cb9e
vagrant <hikarushinomiya2080085@gmail.com>
2015-11-22 15:30:16 +0000

    DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122134858) do
+ActiveRecord::Schema.define(version: 20151122152604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20151122134858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.text     "address"
+    t.text     "detail"
   end
 
   create_table "jobdemands", force: :cascade do |t|
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 20151122134858) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "attr",                                null: false
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
