7f0353c02c18101d44c31bb9b8504c49af590614
duynl_58 <duynl_58@vnu.edu.vn>
2015-11-28 20:08:22 +0700

    remove add_detail_student( redundant DB )

--- db/migrate/20151123061341_add_details_to_students.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :interestedfield, :string
-    add_column :students, :history, :string
-    add_column :students, :achievement, :string
-    add_column :students, :hope, :string
-  end
-end
