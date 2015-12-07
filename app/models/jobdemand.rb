a18d4da187fd813acdc988e898341a40ac9874d1
vagrant <hikarushinomiya2080085@gmail.com>
2015-12-07 14:21:21 +0000

    try fix search function ver3

--- app/models/jobdemand.rb
+++ app/models/jobdemand.rb
@@ -2,4 +2,13 @@ class Jobdemand < ActiveRecord::Base
     belongs_to :company
     
     validates :title, :overview, presence: true
+
+  def self.ransackabole_attributes(auth_oject = nil)
+    %w(tilte overview)
+  end
+
+  def self.sackable_attributes(auth_object = nil)
+    []
+  end
+
 end
